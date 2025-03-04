35f2fe65324ce870f5e5dad451e88b2e9b35f9e9
IsaacBurbidge <114247599+IsaacBurbidge@users.noreply.github.com>
2025-03-04 23:16:31 +0000

    inital stuff for the effect colliders

---
+++ Assets/Scripts/EffectSystem/ApplyEffectsArea.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyEffectsArea : MonoBehaviour
+{
+	[SerializeField]
+	EffectScript effectScript;
+
+	private void OnTriggerEnter(Collider other) {
+		if(other.GetComponent<Ant>() != null) {
+			effectScript.AddEffect(other.GetComponent<Ant>());
+			effectScript.ApplyEffect(other.GetComponent<Ant>());
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.GetComponent<Ant>() != null) {
+			effectScript.RemoveEffect(other.GetComponent<Ant>());
+		}
+	}
+}
