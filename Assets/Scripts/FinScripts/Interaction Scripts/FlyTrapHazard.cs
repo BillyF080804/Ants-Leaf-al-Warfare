2fb09e7210baa8453013b3b5bf63c7e1f2fd4559
Finlay John <findominator7@gmail.com>
2025-03-10 16:18:36 +0000

    Added Venus Flytrap
    Added rigged venus flytrap for usage

---
+++ Assets/Scripts/FinScripts/Interaction Scripts/FlyTrapHazard.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Splines;
+using UnityEngine.UI;
+public class FlyTrapHazard: Interactable
+{
+    [Header("Variables for launch")]
+    [SerializeField] private GameObject attachForSpline;
+    [SerializeField] Quaternion rotation;
+    [SerializeField] Vector3 detachSpeed;
+    public AntScript currentAnt;
+
+    [Header("Animation Variables")]
+    [SerializeField] AnimationHandler uiHandler;
+    [SerializeField] private string animName;
+    [SerializeField] string enterTriggerName;
+    [SerializeField] string exitTriggerName;
+    [SerializeField] Image promptImage;
+    public float imageDistance;
+
+    private SplineAnimate animatedSpline;
+    private AnimationHandler animHandler;
+    private bool isLaunching = false;
+
+
+    private void Awake()
+    {
+        animHandler = GetComponent<AnimationHandler>();
+        animatedSpline = GetComponentInChildren<SplineAnimate>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<AntScript>() != null)
+        {
+            uiHandler.ToggleTrigger(enterTriggerName);
+            currentAnt = other.GetComponent<AntScript>();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(!isLaunching && imageDistance >= 1.5)
+        {
+            imageDistance = Vector2.Distance(promptImage.transform.position, currentAnt.transform.position);
+            promptImage.transform.position = Vector2.MoveTowards(promptImage.transform.position, currentAnt.transform.position, 1 * Time.deltaTime);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((other.GetComponent<AntScript>() == currentAnt) && (currentAnt.transform.parent == null))
+        {
+            uiHandler.ToggleTrigger(exitTriggerName);
+            currentAnt = null;
+        }
+
+    }
+
+    public void AttachObject()
+    {
+        if (currentAnt != null && !isLaunching)
+        {
+            isLaunching = true;
+            uiHandler.ToggleTrigger(exitTriggerName);
+            Rigidbody rb = currentAnt.GetComponent<Rigidbody>();
+            currentAnt.gameObject.transform.SetParent(attachForSpline.transform);
+            currentAnt.transform.rotation = rotation;
+            rb.useGravity = false;
+            animatedSpline.Play();
+            animHandler.ToggleTrigger(animName);
+        }
+    }
+
+    public void DetachObject()
+    {
+        isLaunching = false;
+        Rigidbody rb = currentAnt.GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.AddForce(detachSpeed, ForceMode.Impulse);
+        currentAnt.transform.SetParent(null);
+        currentAnt.transform.rotation = rotation;
+        currentAnt = null;
+        animatedSpline.Restart(false);
+      
+    }
+
+	public override void Interaction() {
+        AttachObject();
+        Debug.Log("Interacted");
+	}
+}
