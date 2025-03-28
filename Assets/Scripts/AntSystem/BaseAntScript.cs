e81eb48b8550b5a9570e7cb92eebbd56be41409f
BillyF080804 <s4205123@glos.ac.uk>
2025-03-28 16:34:37 +0000

    Tweaks/Fixes

--- Assets/Scripts/AntSystem/BaseAntScript.cs
+++ Assets/Scripts/AntSystem/BaseAntScript.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class BaseAntScript : Ant {
     [Header("Base Ant Settings")]
-    [SerializeField] private MeshRenderer hat;
-    [SerializeField] private MeshRenderer leafWrap;
+    [SerializeField] private SkinnedMeshRenderer hat;
+    [SerializeField] private SkinnedMeshRenderer leafWrap;
 
     public void ChangeAntColors(Color newColor) {
         hat.material.SetColor("_MainColours", newColor);
