157919de01186e653b1c1b6da0a8547e1113f743
IsaacBurbidge <114247599+IsaacBurbidge@users.noreply.github.com>
2025-05-03 00:18:05 +0100

    Audio manager created

---
+++ Assets/Scripts/AudioSystem/Audio Manager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	AudioSource[] sources;
+
+	private void Start() {
+		sources = FindObjectsByType<AudioSource>(FindObjectsSortMode.None);
+		for (int i = 0; i < sources.Length; i++) {
+			sources[i].volume = PlayerPrefs.GetFloat("Volume");
+		}
+	}
+}
