2f3361f79736495af6f18c73e46980b4155aad5f
IsaacBurbidge <114247599+IsaacBurbidge@users.noreply.github.com>
2025-05-04 22:46:24 +0100

    Audio Player set up

---
+++ Assets/Scripts/AudioSystem/AudioPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+	[SerializeField] AudioClip audioClip;
+	[SerializeField] AudioSource audioSource;
+
+	private void Start() {
+		audioSource = GetComponent<AudioSource>();
+		audioSource.clip = audioClip;
+	}
+
+	public void PlayClip() {
+		audioSource.Play();
+	}
+
+	public void StopClip() { audioSource.Stop(); }
+
+	public void PauseClip(bool pause) {
+		if (pause) {
+			audioSource.Pause();
+		} else {
+			audioSource.UnPause();
+		}
+	}
+}
